b58855805728182f9b3d4d03799b2966b507bb58
gabrielgua <gabriel.guaita45@gmail.com>
2025-10-14 15:28:57 -0300

    chore: separate top bar into its own component

---
+++ src/components/AnnouncementBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Button from './Button.vue';
+import Container from './Container.vue';
+import Icon from './Icon.vue';
+
+const show = ref(true);
+
+const closeBar = () => {
+  show.value = false;
+};
+
+</script>
+
+<template>
+  <div class="bg-bg-secondary" v-if="show">
+    <Container margin="var(--padding)"
+      class="flex items-center gap-6 text-sm [--padding:1.5rem] md:[--padding:2rem] lg:[--padding:3rem] transition-all">
+      <img src="../assets/images/colors.png" alt="Earplugs Colors"
+        class="h-[60px] w-[120px] object-cover object-top -ms-2" />
+      <p>
+        <span class="font-semibold">Lançamento! </span>
+        <span class="font-semibold text-secondary">NWX</span> Protetores Auriculares Antirruído.
+      </p>
+      <Button variant="secondary" size="xs">
+        Confira já!
+      </Button>
+      <Button @click="closeBar()" variant="neutral" size="xs" class="size-8 p-0! grid place-items-center ml-auto">
+        <Icon icon="xmark" />
+      </Button>
+    </Container>
+  </div>
+</template>
