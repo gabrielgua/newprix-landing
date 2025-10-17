95fb1a05de77ae4d12720632ae6c8f9ac8d0c222
gabrielgua <gabrieljmym13@gmail.com>
2025-10-17 15:33:32 -0300

    feat: add AnnoucementBar component

--- src/components/AnnouncementBar.vue
+++ src/components/AnnouncementBar.vue
@@ -9,25 +9,41 @@ const show = ref(true);
 const closeBar = () => {
   show.value = false;
 };
-
 </script>
 
 <template>
-  <div class="bg-bg-secondary" v-if="show">
-    <Container margin="var(--padding)"
-      class="flex items-center gap-6 text-sm [--padding:1.5rem] md:[--padding:2rem] lg:[--padding:3rem] transition-all">
-      <img src="../assets/images/colors.png" alt="Earplugs Colors"
-        class="h-[60px] w-[120px] object-cover object-top -ms-2" />
-      <p>
-        <span class="font-semibold">Lançamento! </span>
-        <span class="font-semibold text-secondary">NWX</span> Protetores Auriculares Antirruído.
-      </p>
-      <Button variant="secondary" size="xs">
-        Confira já!
-      </Button>
-      <Button @click="closeBar()" variant="neutral" size="xs" class="size-8 p-0! grid place-items-center ml-auto">
-        <Icon icon="xmark" />
-      </Button>
-    </Container>
-  </div>
+  <Transition name="slide-fade" mode="out-in">
+    <div class="bg-bg-secondary transition-all" v-if="show">
+      <Container margin="var(--padding)"
+        class="flex items-center gap-6 text-sm [--padding:1.5rem] md:[--padding:2rem] lg:[--padding:3rem] transition-all">
+        <img src="../assets/images/colors.png" alt="Earplugs Colors"
+          class="h-[60px] w-[120px] object-cover object-top -ms-2" />
+        <p>
+          <span class="font-semibold">Lançamento! </span>
+          <span class="font-semibold text-secondary">NWX</span> Protetores Auriculares Antirruído.
+        </p>
+        <Button variant="secondary" size="xs">
+          Confira já!
+        </Button>
+        <Button @click="closeBar()" variant="neutral" size="xs" class="size-8 p-0! grid place-items-center ml-auto">
+          <Icon icon="xmark" />
+        </Button>
+      </Container>
+    </div>
+  </Transition>
 </template>
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 150ms ease;
+}
+
+.slide-fade-leave-active {
+  transition: all 150ms ease;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-100%);
+  opacity: 0;
+}
+</style>
