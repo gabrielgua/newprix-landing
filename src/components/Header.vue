985178aebd3d8124a6930f051e9042979098c4bf
gabrielgua <gabrieljmym13@gmail.com>
2025-10-27 14:47:02 -0300

    chore: adjust dinamic padding of header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
 import Container from './Container.vue'
 import HeaderLink from './HeaderLink.vue'
 import Logo from './Logo.vue'
+
+const scrolled = ref(false);
+
+const handleScroll = () => {
+  scrolled.value = window.scrollY > 250;
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+
+
 </script>
 
 <template>
-  <header class="bg-bg-base [--padding:1.5rem] md:[--padding:2rem] lg:[--padding:3rem] transition-all"
-    style="padding-block: var(--padding)">
+  <header
+    class="bg-bg-base [--padding:1.5rem] md:[--padding:2rem] lg:[--padding:3rem] transition-all shadow-lg shadow-slate-100"
+    style="padding-block: var(--padding)" :class="{ 'py-6! md:py-8!': scrolled }">
     <Container margin="var(--padding)" class="flex items-center justify-between">
       <Logo />
 
